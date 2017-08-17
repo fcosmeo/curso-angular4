01dda49fff9794d6a8eb53462006196fe18cca5c
fcosmeo <frankcosmesystem@gmail.com>
2017-08-17 17:15:22 -0500

    Hasta Capitulo 28 - NgClass en Atributo

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Empleado } from "./empleado";
 
 @Component({
   selector: 'empleado-tag',
@@ -6,5 +7,36 @@ import { Component } from "@angular/core";
 })
 
 export class EmpleadoComponent{
-  public titulo = 'titulo del componente empleado';
+  public titulo = 'Componente Empleados:';
+  public empleado:Empleado;
+  public trabajadores:Array<Empleado>;
+  public trabajador_externo:boolean;
+  public color:string;
+  public color_seleccionado:string;
+
+  constructor(){
+    this.empleado=new Empleado('Frank Cosme',45,'Cocinero',true );
+    this.trabajadores=[
+      new Empleado('Manolo Martinez',35,'Administrativo',false ),
+      new Empleado('Ana Lopez',78,'Cocinera',true ),
+      new Empleado('Victor Robles',56,'Programador',false)
+    ];
+
+    this.trabajador_externo = true;
+    this.color = 'blue';
+    this.color_seleccionado='#ccc';
+  }
+
+  ngOnInit(){
+    console.log(this.empleado)
+    console.log(this.trabajadores)
+  }
+
+  cambiarExterno(valor){
+    this.trabajador_externo = valor;
+  }
+
+  logColorSeleccionado(){
+    console.log(this.color_seleccionado);
+  }
 }
--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -8,4 +8,42 @@ import { Component } from "@angular/core";
 export class FrutaComponent{
   public nombre_componente='Componente de fruta';
   public listado_frutas='Naranja, Manzana, Pera y Sandia';
+
+  public nombre:string;
+  public edad:number;
+  public mayorDeEdad:boolean;
+  public trabajos:Array<any>=['Carpintero',22,'Fontanero']; //any para cualquier tipo de dato
+  comodin:any;
+
+  constructor(){
+    this.nombre='Frank Cosme';
+    this.edad=30;
+    this.mayorDeEdad=false;
+    this.comodin='Si';
+
+  }
+  //Metodo por defecto que se lanza despues del constructor
+  ngOnInit(){
+    this.cambiarNombre();
+    this.cambiarEdad(35);
+    console.log(this.nombre+" "+this.edad);
+
+    //Variables y alcance
+    var uno = 8;
+    var dos = 15;
+
+    if(uno=8){
+      let uno=3;
+      var dos=88;
+      console.log('DENTRO DEL IF: '+uno+" "+dos);
+    }
+    console.log('FUERA DEL IF: '+uno+" "+dos);
+  }
+
+  cambiarNombre(){
+    this.nombre='Juan Lopez';
+  }
+  cambiarEdad(edad){
+    this.edad=edad;
+  }
 }
